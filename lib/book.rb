6f5743d22c7a64386ab9262b2b7a0309488669d9
Mehwish Aziz <mehwisha@msn.com>
2018-05-04 02:18:34 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,18 +1,20 @@
 class Book
-def initialize(title)
-@title = title
-end
-def title
-@title
-end
-def author=(author)
-@author = author
-end
-def author
-@author
-end
 
+  def initialize(title)
+    @title = title
+  end
 
+  def title
+    @title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
 
 end
 Book.new("And Then There Were None")
