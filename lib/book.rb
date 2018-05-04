ddac30aa4ed30282a1d46e16f35a4f11b618e16c
Mehwish Aziz <mehwisha@msn.com>
2018-05-04 02:20:37 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -18,19 +18,19 @@ class Book
   def page_count=(num)
       @page_count = num
     end
-   
+
     def page_count
       @page_count
     end
-   
+
     def genre=(genre)
       @genre = genre
     end
-   
+
     def genre
       @genre
     end
-   
+
     def turn_page
     end
 end
